35cf46184407aaf12e15cba694e1bdfe4eda3e69
EAG <pablo.alcalde.garcia@gmail.com>
2024-01-25 20:00:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/seminario3di2024/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.seminario3di2024
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.temporizador)
+        val countDownTimer = object : CountDownTimer(1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                textView.text = (millisUntilFinished / 1000).toString()
+            }
+
+            override fun onFinish() {
+                var newIntent = Intent(this@MainActivity, Ejercicio3::class.java)
+                startActivity(newIntent)
+            }
+        }
+        countDownTimer.start()
+    }
+}
